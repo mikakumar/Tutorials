5da7addc5bc4bf4abd0044f79cc9cbd1e6155ac0
mikakumar <mihika02@hotmail.com>
2020-02-07 17:43:50 +0100

    angular experiment 01

--- Angular/exapp/src/app/app.module.ts
+++ Angular/exapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterAppComponent } from './components/counter-app/counter-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterAppComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/exapp/src/app/components/counter-app/counter-app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-app',
+  templateUrl: './counter-app.component.html',
+  styleUrls: ['./counter-app.component.css']
+})
+export class CounterAppComponent implements OnInit {
+
+  constructor() { }
+
+  counter: number;
+
+  ngOnInit(): void{
+    this.counter = 0;
+  }
+
+  countUp() {
+    this.counter++;
+  }
+  countDown(){
+    if(this.counter == 0){
+    this.counter = 0;
+    }
+    else {
+    this.counter--;
+    }
+  }
+
+}
