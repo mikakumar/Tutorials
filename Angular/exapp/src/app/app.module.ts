01d603bd20cb93c7e3dec4768d8f64a76d4f86a2
mikakumar <mihika02@hotmail.com>
2020-02-11 23:41:22 +0100

    experiment time

--- Angular/exapp/src/app/app.module.ts
+++ Angular/exapp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterAppComponent } from './components/counter-app/counter-app.component';
+import { PictureComponent } from './components/picture/picture.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterAppComponent
+    CounterAppComponent,
+    PictureComponent
   ],
   imports: [
     BrowserModule,
