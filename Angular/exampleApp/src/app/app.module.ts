9dd84c9a271aa620e10dc471b7d1b24abb68a191
mikakumar <mihika02@hotmail.com>
2020-03-12 22:46:19 +0100

    add +extra and codepen trials

--- Angular/exampleApp/src/app/app.module.ts
+++ Angular/exampleApp/src/app/app.module.ts
@@ -3,26 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './components/counter/counter.component';
-import { PhonebookComponent } from './components/phonebook/phonebook.component';
-import { LandingComponent } from './components/landing/landing.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { HeaderComponent } from './components/header/header.component';
-import { IndexComponent } from './components/index/index.component';
-import { LoadingComponent } from './components/loading/loading.component';
-import { PersonaLandingComponent } from './components/persona-landing/persona-landing.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
-    PhonebookComponent,
-    LandingComponent,
-    SidebarComponent,
-    HeaderComponent,
-    IndexComponent,
-    LoadingComponent,
-    PersonaLandingComponent
+    CounterComponent
   ],
   imports: [
     BrowserModule,
